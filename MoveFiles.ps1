416a9dae121a53a1bdf51f9b3c64b1353c5101a8
AwaisD <107219381+imawais004@users.noreply.github.com>
2023-01-26 09:22:08 -0800

    Update MoveFiles.ps1

--- MoveFiles.ps1
+++ MoveFiles.ps1
@@ -1,10 +1,8 @@
 # Create a new scheduled task trigger
-$trigger = New-ScheduledTaskTrigger -Daily -At "10:00"
+$trigger = New-JobTrigger -At 10:00 -Daily
 
 # Register the script as a scheduled task
-Register-ScheduledTask -TaskName "Move Files Task" -Trigger $trigger -User "NT AUTHORITY\SYSTEM" -ScriptBlock {
-    # Script to move files
-    foreach($fileType in $fileTypes){
-        Get-ChildItem -Path D:\Folder -Filter "$fileType*.txt" -Recurse | Sort-Object LastWriteTime | Select-Object -Skip 10 | Move-Item -Destination <Path>
+Register-ScheduledJob -Name 'Move Files Daily' -Trigger $trigger -ScriptBlock {
+    # Script to move files, skips top10 files
+            Get-ChildItem -Path D:\Folder -Filter "$fileType*.txt" -Recurse | Sort-Object LastWriteTime | Select-Object -Skip 10 | Move-Item -Destination <Path>
     }
-}
